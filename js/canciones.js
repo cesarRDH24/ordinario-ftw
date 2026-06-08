09901b3d413d83eedc72e1630678dbad70673ce9
Cesar Hernández <cesar4rodrigo@gmail.com>
2026-06-08 08:32:04 -0600

    detalles y documentacion

--- js/canciones.js
+++ js/canciones.js
@@ -1,3 +1,5 @@
+let todasLasCanciones = [];
+
 fetch("../xml/canciones.xml")
 .then(response => response.text())
 .then(data => {
@@ -16,32 +18,93 @@ fetch("../xml/canciones.xml")
             "cancion"
         );
 
+    for(let cancion of canciones)
+    {
+        todasLasCanciones.push({
+
+            titulo:
+                cancion
+                .getElementsByTagName("titulo")[0]
+                .textContent,
+
+            artista:
+                cancion
+                .getElementsByTagName("artista")[0]
+                .textContent,
+
+            duracion:
+                cancion
+                .getElementsByTagName("duracion")[0]
+                .textContent
+
+        });
+    }
+
+    mostrarCanciones(
+        todasLasCanciones
+    );
+
+});
+
+function mostrarCanciones(lista)
+{
     let tabla =
         document.getElementById(
             "tablaCanciones"
         );
 
-    for(let cancion of canciones)
-    {
+    tabla.innerHTML = "";
+
+    lista.forEach(cancion => {
+
         tabla.innerHTML += `
 
         <tr>
 
-            <td>
-                ${cancion.getElementsByTagName("titulo")[0].textContent}
-            </td>
+            <td>${cancion.titulo}</td>
 
-            <td>
-                ${cancion.getElementsByTagName("artista")[0].textContent}
-            </td>
+            <td>${cancion.artista}</td>
 
-            <td>
-                ${cancion.getElementsByTagName("duracion")[0].textContent}
-            </td>
+            <td>${cancion.duracion}</td>
 
         </tr>
 
         `;
-    }
 
-});
+    });
+}
+
+function filtrarCanciones()
+{
+    let texto =
+        document
+        .getElementById("busquedaCancion")
+        .value
+        .toLowerCase();
+
+    let filtradas =
+        todasLasCanciones.filter(
+            cancion =>
+
+            cancion.titulo
+            .toLowerCase()
+            .includes(texto)
+
+            ||
+
+            cancion.artista
+            .toLowerCase()
+            .includes(texto)
+        );
+
+    mostrarCanciones(
+        filtradas
+    );
+}
+
+document
+.getElementById("busquedaCancion")
+.addEventListener(
+    "keyup",
+    filtrarCanciones
+);
